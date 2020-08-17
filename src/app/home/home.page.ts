b6534c5e973c68b3d106e4b3c6d590dc7193b1a4
Abraham Olaobaju <olaobajua@gmail.com>
2020-08-17 20:42:40 +0100

    Final Review

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ThemeableBrowser, ThemeableBrowserOptions, ThemeableBrowserObject } from '@ionic-native/themeable-browser/ngx';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private themeableBrowser: ThemeableBrowser) {
+    this.openBrowser();
+  }
+
+  openBrowser(){
+    const options: ThemeableBrowserOptions = {
+      toolbar: {
+        height: 0,
+        color: '#ffffff'
+    }
+      
+ }
+    const browser: ThemeableBrowserObject = this.themeableBrowser.create('https://www.tclilibrary.com','_blank', options);
+  }
+
+  
 
 }
